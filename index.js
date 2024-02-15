fb5c196e4c7dbf0f88582c7b6a3a29af42e4462e
SarahVanT <sarahvantilburg1@outlook.com>
2024-02-15 13:23:45 -0500

    Changed the swiping feature on the carousel

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var startX, endX;
-  // Selecting #carouselExample
   var carouselElement = document.querySelector("#carouselExample");
 
-  // Event listener when user touches the screen
+  // Ensure the carousel is initialized
+  var carouselInstance = new bootstrap.Carousel(carouselElement);
+
   carouselElement.addEventListener("touchstart", function (e) {
-    // horizontal position (X-coordinate) of the touch point
     startX = e.touches[0].clientX;
   });
 
-  // Event listener when user moves finger across the screen
   carouselElement.addEventListener(
     "touchmove",
     function (e) {
@@ -18,17 +17,15 @@ document.addEventListener("DOMContentLoaded", function () {
     { passive: false }
   );
 
-  // Event listener when user lifts finger off the screen
   carouselElement.addEventListener("touchend", function (e) {
-    // horizontal position (X-coordinate) of the touch ended point
     endX = e.changedTouches[0].clientX;
 
     if (startX - endX > 50) {
       // Swipe left, next slide
-      $(".carousel").carousel("next");
+      carouselInstance.next();
     } else if (endX - startX > 50) {
       // Swipe right, previous slide
-      $(".carousel").carousel("prev");
+      carouselInstance.prev();
     }
   });
 });
